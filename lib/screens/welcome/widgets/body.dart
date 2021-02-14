6c1a35312aeaf9456cffd4229555f9a7e35b06a7
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-02-14 21:57:20 +0800

    Add Welcome screen

---
+++ lib/screens/welcome/widgets/body.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../constants.dart';
+import './background.dart';
+import './rounded_button.dart';
+
+class Body extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // size provides the total height and width of the screen.
+    final Size size = MediaQuery.of(context).size;
+
+    return SingleChildScrollView(
+      child: Background(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Welcome to\nPabitit Pabagahe Pabili',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.purple,
+                wordSpacing: 2,
+                letterSpacing: 0.5,
+              ),
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            SvgPicture.asset(
+              'assets/icons/chat.svg',
+              height: size.height * 0.45,
+            ),
+            SizedBox(
+              height: size.height * 0.05,
+            ),
+            RoundedButtons(
+              text: 'LOGIN',
+              press: () {},
+            ),
+            RoundedButtons(
+              text: 'SIGN UP',
+              color: kPrimaryLightColor,
+              textColor: Colors.black,
+              press: () {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
