8daad5d52bd806974fb26b4ee36b9bb50c1b49de
Marc Peejay Viernes <mpvviernes@gmail.com>
2021-02-20 20:47:00 +0800

    Add Theme data - fonts and colors

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,39 @@
 import 'package:flutter/material.dart';
 
-import './constants.dart';
-import './screens/welcome/welcome_screen.dart';
-
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of the application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Pabitbit Pabagahe Pabili',
+      title: 'Pabitbit Pabagahe Pabili (P3)',
       theme: ThemeData(
-        primaryColor: kPrimaryColor,
-        scaffoldBackgroundColor: Colors.white,
+        primarySwatch: Colors.purple,
+        accentColor: const Color.fromRGBO(248, 161, 209, 1.0),
+        fontFamily: 'ProximaNova',
+        textTheme: const TextTheme(
+          headline4: TextStyle(fontSize: 26.0, fontWeight: FontWeight.bold),
+          headline5: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          headline6: TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold),
+          bodyText1: TextStyle(fontSize: 16.0),
+        ),
       ),
-      home: WelcomeScreen(),
+      home: MyHome(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(
+          'Hello',
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ),
     );
   }
 }
